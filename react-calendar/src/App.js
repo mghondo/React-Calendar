e73f5fe2c614137af9fcff30d20515cfd33735c0
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-08-04 19:50:15 -0400

    first commit

---
+++ react-calendar/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>What it is right now!</h1>
+    </div>
+  );
+}
+
+export default App;
