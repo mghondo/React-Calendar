b0d7d9184b3e71d90331a73a79d99063cdc09244
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-08-05 22:36:48 -0400

    Date outputting at the bottom of the screen.

--- react-calendar/src/App.js
+++ react-calendar/src/App.js
@@ -5,9 +5,16 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 function App() {
+  const [date, setDate] = useState(new Date());
   return (
     <div className="App">
-      <h1>What it is right now!</h1>
+      <h1 className="text-center">React Calendar</h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className="text-center">
+        <span className="bold">Selected Date:</span> {date.toDateString()}
+      </p>
     </div>
   );
 }
